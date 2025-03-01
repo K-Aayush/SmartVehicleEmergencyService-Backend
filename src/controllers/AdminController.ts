2adc9551253cf6d676affc4083627603ff04e757
Aayush <ayushkarkee6@gmail.com>
2025-03-01 22:39:06 +0545

    update :: made api for total users based on role

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -6,6 +6,7 @@ interface AuthenticatedRequest extends Request {
   user?: any;
 }
 
+//get all users
 export const getAllUsers = async (req: AuthenticatedRequest, res: Response) => {
   const userId = req.user?.id;
   const userRole = req.user?.role;
@@ -31,6 +32,31 @@ export const getAllUsers = async (req: AuthenticatedRequest, res: Response) => {
     });
 
     res.status(200).json({ success: true, users });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getTotalNoOfUsers = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  const userId = req.user?.id;
+  const userRole = req.user?.role;
+
+  if (!userId || userRole !== "ADMIN") {
+    res.status(401).json({ success: false, message: "Unauthorized access" });
+    return;
+  }
+
+  const { role } = req.query;
+
+  try {
+    const totalUsers = await db.user.count({
+      where: role ? { role: role as Role } : {},
+    });
+
+    res.status(200).json({ success: true, totalUsers });
   } catch (error) {
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
--- src/routes/AdminRoutes.ts
+++ src/routes/AdminRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
-import { getAllUsers } from "../controllers/AdminController";
+import { getAllUsers, getTotalNoOfUsers } from "../controllers/AdminController";
 
 const router = express.Router();
 
 router.get("/getAllUsers", authMiddleware, getAllUsers);
+router.get("/getTotalUsers", authMiddleware, getTotalNoOfUsers);
 
 export default router;
